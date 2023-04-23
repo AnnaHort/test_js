852a7f972c6a24de90bca8f9dcdbb3b26ad53511
AnnaHort <annahort3d@gmail.com>
2023-04-23 18:30:33 +0300

    test1

---
+++ test.js
@@ -0,0 +1,6 @@
+const clients = ["Mango", "Poly", "Ajax"];
+
+// Зазначаючи в дужках індекс елемента, ми отримуємо його значення
+console.log(clients[0]); // Mango
+console.log(clients[1]); // Poly
+console.log(clients[2]); // Ajax
