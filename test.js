a2ed1ea8f44d2f3cca288f1a82bd3aec18a7f424
AnnaHort <annahort3d@gmail.com>
2023-04-25 22:04:36 +0300

    module 2 (25.04)

--- test.js
+++ test.js
@@ -57,3 +57,53 @@
 //   console.log(slugify("English for developer"));
 //   console.log(slugify("Ten secrets of JavaScript"));
 //   console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+
+
+// function makeArray(firstArray, secondArray, maxLength) {
+
+//     // firstArray - перший масив
+//     // secondArray - другий масив
+//     // maxLength - максимально допустима довжина нового масиву
+
+//     // 1. якщо кількість елементів нового масиву більша за maxLength то треба
+//     // повернути копію масиву довжиною maxLength 
+
+//     // 2. в іншому випадку повернути новий масив повністю
+
+//     const newMasive = firstArray.concat(secondArray);
+//     // сума елементів масиву
+//     const length = newMasive.length; 
+//     // кількість елементів масиву
+//     const masiveElement = length > maxLength ? newMasive.slice(maxLength) : newMasive;
+
+
+//     return masiveElement;
+  
+//   }
+
+// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4) );
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
+
+
+// function calculateTotal(number) {
+// //  функція приймає ціле число number
+// // 1. Повертає суму всіх цілих числел від 1 до number
+// let sum = 0;
+
+// for (let i = 1; i < number; i += 1) {
+// sum += i; 
+// }
+
+// return sum;
+//    }
+
+//    console.log(calculateTotal(1));
+//    console.log(calculateTotal(3));
+//    console.log(calculateTotal(7));
+//    console.log(calculateTotal(18));
+//    console.log(calculateTotal(24));
+//    console.log(calculateTotal());
